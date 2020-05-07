0fb2358fbabf7957bcf330ae73e152e2fcf90969
Alan Lee <alanleeone@gmail.com>
2020-05-07 09:08:19 -0700

    pull

--- access/againstCard.js
+++ access/againstCard.js
@@ -135,3 +135,19 @@ document.getElementById('searchBtn').addEventListener('click', (event) => {
       });
   }
 });
+
+document.getElementById('tcgLink').addEventListener('click', (e) => {
+  e.preventDefault()
+
+  fetch(`https://api.pokemontcg.io/v1/cards?name=${document.getElementById('search').value}`)
+    .then(r => r.json())
+    .then(pokemonData => {
+      let cardIndex = 0
+      // console.log(pokemonData)
+      // console.log(pokemonData.cards[0])
+      document.getElementById('tcgLink').innerHTML =
+        `
+    <img src='${pokemonData.cards[Math.floor(Math.random() * pokemonData.cards.length)].imageUrl}'>
+    `
+    })
+})
--- access/script.js
+++ access/script.js
@@ -9,60 +9,6 @@ function checkForDirectLink() {
 }
 checkForDirectLink();
 
-<<<<<<< HEAD
-//         <li class="main-card-list-item-ability">
-//             abilities: ${pokemon.abilities[0].ability.name},
-//             ${pokemon.abilities[1].ability.name}
-//         </li>
-//         <li class="main-card-list-item-stats0">
-//             ${pokemon.stats[0].stat.name}: ${pokemon.stats[0].base_stat}
-//         </li>
-//         <li class="main-card-list-item-stats1">
-//             ${pokemon.stats[1].stat.name}: ${pokemon.stats[1].base_stat}
-//         </li>
-//         <li class="main-card-list-item-stats2">
-//             ${pokemon.stats[2].stat.name}: ${pokemon.stats[2].base_stat}
-//         </li>
-//         <li class="main-card-list-item-stats3">
-//             ${pokemon.stats[3].stat.name}: ${pokemon.stats[3].base_stat}
-//         </li>
-//         <li class="main-card-list-item-stats4">
-//             ${pokemon.stats[4].stat.name}: ${pokemon.stats[4].base_stat}
-//         </li>
-//         <li class="main-card-list-item-stats6">
-//             ${pokemon.stats[5].stat.name}: ${pokemon.stats[5].base_stat}
-//         </li>
-
-//         document.getElementById('nameInput').value = '';
-//         document.getElementById('body-main-pokemon').innerHTML = '';
-//         document.getElementById('body-main-pokemon').append(pokeElem);
-//       })
-//       .catch((e) => {
-//         console.log(e);
-//         //removed the alert due to the requirement.
-//         document.getElementById('nameInput').value = '';
-//         document.getElementById('body-main-pokemon').innerHTML = '';
-//       });
-//   }
-// });
-
-
-// line 57 card 
-document.getElementById('tcgLink').addEventListener('click', (e) => {
-  e.preventDefault()
-
-  fetch(`https://api.pokemontcg.io/v1/cards?name=${document.getElementById('search').value}`)
-    .then(r => r.json())
-    .then(pokemonData => {
-      let cardIndex = 0
-      // console.log(pokemonData)
-      // console.log(pokemonData.cards[0])
-      document.getElementById('tcgLink').innerHTML =
-        `
-    <img src='${pokemonData.cards[Math.floor(Math.random() * pokemonData.cards.length)].imageUrl}'>
-    `
-    })
-})
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -76,7 +22,6 @@ document.addEventListener('DOMContentLoaded', function () {
 // git add
 // git commit
 // git push origin(branchname)
-=======
 document.getElementById('searchPokemon').addEventListener('click', (event) => {
   event.preventDefault();
   let inputValue = document.getElementById('nameInput').value;
@@ -161,4 +106,22 @@ function getAllPokemon() {
   }
 }
 getAllPokemon();
->>>>>>> 050d936e02b005b65cacd53e70156edaa838e19c
+
+
+
+
+document.getElementById('tcgLink').addEventListener('click', (e) => {
+  e.preventDefault()
+
+  fetch(`https://api.pokemontcg.io/v1/cards?name=${document.getElementById('search').value}`)
+    .then(r => r.json())
+    .then(pokemonData => {
+      let cardIndex = 0
+      // console.log(pokemonData)
+      // console.log(pokemonData.cards[0])
+      document.getElementById('tcgLink').innerHTML =
+        `
+    <img src='${pokemonData.cards[Math.floor(Math.random() * pokemonData.cards.length)].imageUrl}'>
+    `
+    })
+})
